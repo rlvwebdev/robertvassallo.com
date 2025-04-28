37a0fa645cc27e5e11412e3248d3b2c8951fc5c7
Robert Vassallo <rlvwebdev@gmail.com>
2025-04-27 21:48:00 -0400

    updating folio

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./core/**/*.{html,js}",
+    "./artwork/**/*.{html,js}",
+    "./blog/**/*.{html,js}",
+    "./contact/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
